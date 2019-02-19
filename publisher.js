2f508aa3c304c8f212eb1fad36860bb28f9d0455
hnamzian <namazian87@gmail.com>
2019-02-19 12:36:42 +0330

    test publisher done

--- publisher.js
+++ publisher.js
@@ -2,30 +2,39 @@ const moment = require("moment");
 var mqtt = require("mqtt");
 var client = mqtt.connect("mqtt://localhost:1881");
 
+const parlourName = "Mylko";
+const sectionName = "Milking";
+const unitName = "u-1";
+
+const tags = [
+  "211.244.225.84.120",
+  "211.244.225.84.130",
+  "211.244.225.84.140",
+  "211.244.225.84.150",
+  "211.244.225.84.160"
+];
+
+let data = {
+  chipId: "80:7D:3A:45:59:00",
+  voltage: 33,
+  temperature: 308,
+  ambientTemp: 235,
+  volume: 3659,
+  macAddress: "21:34:53:89:31",
+  tagNumber: "",
+  date: ""
+};
+
 client.on("connect", function() {
-  //   client.subscribe("milking/response", (topic, message) => {
-  //     console.log(topic, message);
-  //   });
-  let dateOffset = 0;
-  setInterval(function() {
-    const parlourName = "Mylko";
-    const sectionName = "Milking";
-    const unitName = "u-1";
-    date = moment()
-      .add(dateOffset, "days")
-      .format();
-    const data = {
-      chipId: "80:7D:3A:45:59:00",
-      voltage: 33,
-      temperature: 308,
-      ambientTemp: 235,
-      volume: 3659,
-      tagNumber: "211.244.225.84.129",
-      macAddress: "21:34:53:89:31",
-      createdAt: date
-    };
-    client.publish(`milking/${parlourName}/${sectionName}/${unitName}`, JSON.stringify(data));
-    console.log(date);
-    dateOffset++;
-  }, 1000);
+
+  for (let dateOffset=0; dateOffset < 800; dateOffset++) {
+    for (let tag of tags) {
+      let date = moment().add(dateOffset, "days").format();
+      data.tagNumber = tag;
+      data.date = date;
+      client.publish(`milking/${parlourName}/${sectionName}/${unitName}`, JSON.stringify(data));
+      console.log(data);
+    }
+  }
+    
 });
