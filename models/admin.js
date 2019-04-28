14554e0e833c5012a974d185184fc1bebe92fede
hnamzian <namazian87@gmail.com>
2019-04-28 19:10:42 +0430

    mobile column's allowance to null set as false

--- models/admin.js
+++ models/admin.js
@@ -15,7 +15,7 @@ module.exports = (sequelize, Sequelize) => {
     },
     mobile: {
       type: Sequelize.STRING,
-      allowNull: true
+      allowNull: false
     },
     email: {
       type: Sequelize.STRING,
