3f00c7c3a85eacf2702cf3f68f619fe40066c60e
hnamzian <namazian87@gmail.com>
2019-04-28 18:28:17 +0430

    wool model added

---
+++ models/wool.js
@@ -0,0 +1,22 @@
+module.exports = (sequelize, Sequelize) => {
+  return sequelize.define("Wool", {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    volume: {
+      type: Sequelize.INTEGER,
+      allowNull: false
+    },
+    // animalId: {
+    //   type: Sequelize.INTEGER,
+    //   allowNull: false
+    // },
+    date: {
+      type: Sequelize.DATE,
+      defaultValue: Sequelize.NOW,
+      allowNull: false
+    }
+  });
+};
