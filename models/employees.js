a03cb3be453c175bbff98af58db7514800e2c06f
hnamzian <namazian87@gmail.com>
2019-04-28 18:31:25 +0430

    employees model added

---
+++ models/employees.js
@@ -0,0 +1,30 @@
+module.exports = (sequelize, Sequelize) => {
+  return sequelize.define("User", {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    firstName: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    lastName: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    mobile: {
+      type: Sequelize.STRING,
+      allowNull: true
+    },
+    email: {
+      type: Sequelize.STRING,
+      allowNull: true
+    },
+    role: {
+      type: Sequelize.ENUM,
+      values: ["ADMIN", "EMPLOYEE"],
+      allowNull: false
+    }
+  });
+};
