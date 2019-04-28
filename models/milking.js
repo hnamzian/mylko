de4292c93795c6a6a3f9a62db59c9eac25695e1b
hnamzian <namazian87@gmail.com>
2019-04-28 18:34:15 +0430

    milking model module linted

--- models/milking.js
+++ models/milking.js
@@ -5,15 +5,41 @@ module.exports = (sequelize, Sequelize) => {
       primaryKey: true,
       autoIncrement: true
     },
-    chipId: { type: Sequelize.STRING },
-    voltage: { type: Sequelize.INTEGER },
-    temperature: { type: Sequelize.INTEGER },
-    ambientTemp: { type: Sequelize.INTEGER },
-    volume: { type: Sequelize.INTEGER },
-    parlourName: { type: Sequelize.STRING },
-    sectionName: { type: Sequelize.STRING },
-    unitName: { type: Sequelize.STRING },
-    tagNumber: { type: Sequelize.STRING },
+    chipId: {
+      type: Sequelize.STRING, 
+      allowNull: false },
+    voltage: { 
+      type: Sequelize.INTEGER, 
+      allowNull: false 
+    },
+    temperature: { 
+      type: Sequelize.INTEGER, 
+      allowNull: false 
+    },
+    ambientTemp: { 
+      type: Sequelize.INTEGER, 
+      allowNull: false 
+    },
+    volume: { 
+      type: Sequelize.INTEGER, 
+      allowNull: false 
+    },
+    parlourName: {
+      type: Sequelize.STRING, 
+      allowNull: false 
+    },
+    sectionName: {
+      type: Sequelize.STRING, 
+      allowNull: false 
+    },
+    unitName: {
+      type: Sequelize.STRING, 
+      allowNull: false 
+    },
+    tagNumber: {
+      type: Sequelize.STRING, 
+      allowNull: false 
+    }
     // date: { type: Sequelize.DATE, defaultValue: Sequelize.NOW }
   });
 };
