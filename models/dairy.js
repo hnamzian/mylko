650a9d857d28771efb5feeb6a3e4fad28fe13c58
hnamzian <namazian87@gmail.com>
2019-04-29 13:42:35 +0430

    dairy model added

---
+++ models/dairy.js
@@ -0,0 +1,17 @@
+module.exports = (sequelize, Sequelize) => {
+  return sequelize.define("Dairy", {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    address: {
+      type: Sequelize.STRING,
+      allowNull: false
+    }
+  });
+};
