0d0baf4627d4ffa22d437ec729d531832a2113da
hnamzian <namazian87@gmail.com>
2019-04-29 14:05:17 +0430

    admin route introduced as express middleware

--- startup/routes.js
+++ startup/routes.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const deviceRouter = require("../routes/device");
 const milkingRouter = require("../routes/milking");
+const adminRouter = require("../routes/admin");
 
 module.exports = function(app) {
   app.use(express.json());
   app.use("/device", deviceRouter);
   app.use("/milking", milkingRouter);
+  app.use("/admin", adminRouter);
 };
