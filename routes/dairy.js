ff6c8d31d8c22d912b15978748c2b523d0af79eb
hnamzian <namazian87@gmail.com>
2019-04-29 13:54:19 +0430

    route module dairy initialized

---
+++ routes/dairy.js
@@ -0,0 +1,3 @@
+const { Dairy } = require("../startup/db");
+const express = require("express");
+router = express.Router();
---
+++ routes/disease.js
@@ -0,0 +1,3 @@
+const { Disease } = require("../startup/db");
+const express = require("express");
+router = express.Router();
