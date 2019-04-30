e20ae477f81db762ce8213b55579d2619c45fa39
hnamzian <namazian87@gmail.com>
2019-04-30 21:10:01 +0430

    add server address as mqtt broker address

--- playground/publisher.js
+++ playground/publisher.js
@@ -1,18 +1,12 @@
 const moment = require("moment");
 var mqtt = require("mqtt");
-var client = mqtt.connect("mqtt://localhost:1881");
+var client = mqtt.connect("mqtt://176.9.174.30:1881");
 
 const parlourName = "Mylko";
 const sectionName = "Milking";
 const unitName = "u-1";
 
-const tags = [
-  "211.244.225.84.120",
-  "211.244.225.84.130",
-  "211.244.225.84.140",
-  "211.244.225.84.150",
-  "211.244.225.84.160"
-];
+const tags = ["211.244.225.84.120", "211.244.225.84.130", "211.244.225.84.140", "211.244.225.84.150", "211.244.225.84.160"];
 
 let data = {
   chipId: "80:7D:3A:45:59:00",
@@ -26,16 +20,15 @@ let data = {
 };
 
 client.on("connect", function() {
-
-  for (let dateOffset=0; dateOffset < 800; dateOffset++) {
+  for (let dateOffset = 0; dateOffset < 800; dateOffset++) {
     for (let tag of tags) {
-      
-      let date = moment().add(dateOffset, "days").format();
+      let date = moment()
+        .add(dateOffset, "days")
+        .format();
       data.tagNumber = tag;
       data.date = date;
       client.publish(`milking/${parlourName}/${sectionName}/${unitName}`, JSON.stringify(data));
       console.log(data);
     }
   }
-    
 });
