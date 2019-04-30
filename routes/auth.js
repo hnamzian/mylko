bd1a2c082edb39b1dd0961e62afd18850e72431a
hnamzian <namazian87@gmail.com>
2019-04-30 22:04:32 +0430

    add initilized auth route module

---
+++ routes/auth.js
@@ -0,0 +1,9 @@
+const _ = require("lodash");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const express = require("express");
+router = express.Router();
+
+router.post("/", async (req, resp) => {});
+
+module.exports = router;
