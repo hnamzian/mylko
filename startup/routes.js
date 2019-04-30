2be000e4dc11cc9d4a9502656e2cb524f70d685e
hnamzian <namazian87@gmail.com>
2019-04-30 21:51:44 +0430

    add error middleware to express to as error handler middleware

--- startup/routes.js
+++ startup/routes.js
@@ -1,11 +1,14 @@
 const express = require("express");
+require("express-async-errors");
 const deviceRouter = require("../routes/device");
 const milkingRouter = require("../routes/milking");
 const adminRouter = require("../routes/admin");
+const error = require("../middleware/error");
 
 module.exports = function(app) {
   app.use(express.json());
   app.use("/device", deviceRouter);
   app.use("/milking", milkingRouter);
   app.use("/admin", adminRouter);
+  app.use(error);
 };
