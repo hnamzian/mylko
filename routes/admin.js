67b4d7f4d47504c49dfdbe7574dd030b4248f5dc
hnamzian <namazian87@gmail.com>
2019-05-02 15:49:12 +0430

    extract and throw error message at registering new admin

--- routes/admin.js
+++ routes/admin.js
@@ -13,15 +13,19 @@ router.post("/register", async (req, resp) => {
 
   const { error } = validate(admin);
   if (error) return resp.status(400).send("Joi: " + error.details[0].message);
-  
+
   const salt = await bcrypt.genSalt(10);
   const hashed = await bcrypt.hash(admin.password, salt);
   admin.password = hashed;
 
-  const result = await Admin.create(admin);
-
-  const token = jwt.sign({ _id: result.dataValues.id, isAdmin: this.isAdmin }, config.jwtPrivateKey);
-  resp.send(token);
+  try {
+    const result = await Admin.create(admin);
+    const token = jwt.sign({ _id: result.dataValues.id, isAdmin: this.isAdmin }, config.jwtPrivateKey);
+    resp.send(token);
+  } catch (ex) {
+    winston.debug(ex.errors[0].message);
+    throw ex.errors[0].message;
+  }
 });
 
 function validate(admin) {
@@ -41,7 +45,7 @@ function validate(admin) {
       .max(255)
       .required()
       .email(),
-    address: Joi.string(),
+    address: Joi.string().min(0),
     password: Joi.string()
       .min(5)
       .max(1024)
