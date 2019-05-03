04aeca47fbee9033a3c902aa8335e12fb166c205
hnamzian <namazian87@gmail.com>
2019-05-03 18:40:14 +0430

    bug fixed at auth middleware module to pass jwtPrivateKey to token verificatin and returning _id as result

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,12 +1,15 @@
 const jwt = require("jsonwebtoken");
+const config = require("config");
 
 module.exports = function(req, res, next) {
   const token = req.header("x-auth-token");
+  console.log(token);
   if (!token) return res.status(401).send("Access denied. token not provided");
 
   try {
-    const decodedToken = jwt.verify(token, "privatekey");
-    req.user = decodedToken;
+    const decodedToken = jwt.verify(token, config.get("jwtPrivateKey"));
+    req.userId = decodedToken._id;
+
     next();
   } catch (ex) {
     res.status(400).send("invalid token");
