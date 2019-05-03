c2fa394572f3aa7805cb2fe6c79d7e310606a966
hnamzian <namazian87@gmail.com>
2019-05-03 17:58:10 +0430

    add validate method to routes/employees module to validate request data on employees/add route

--- routes/employees.js
+++ routes/employees.js
@@ -9,13 +9,44 @@ router = express.Router();
 router.post("/add", async (req, resp) => {
   const employee = _.pick(req.body, ["firstName", "lastName", "mobile", "email", "address", "position", "DairyId"]);
 
+  const { error } = validate(employee);
+  if (error) return resp.status(400).send("Joi: " + error.details[0].message);
+
   try {
     const result = await Employees.create(employee);
     resp.send(result);
   } catch (ex) {
-    console.log(ex)
+    console.log(ex);
     throw ex;
   }
 });
+
+function validate(employee) {
+  const schema = {
+    firstName: Joi.string()
+      .min(2)
+      .required(),
+    lastName: Joi.string()
+      .min(2)
+      .required(),
+    mobile: Joi.string()
+      .min(11)
+      .max(13)
+      .required(),
+    email: Joi.string()
+      .min(5)
+      .max(255)
+      .required()
+      .email(),
+    address: Joi.string().min(0),
+    position: Joi.string()
+      .min(5)
+      .max(255)
+      .required(),
+    DairyId: Joi.number()
+  };
+
+  return Joi.validate(employee, schema);
+}
 
 module.exports = router;
