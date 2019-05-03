9d98cdd680c9d6b633dd19c6cf9ac88c07349597
hnamzian <namazian87@gmail.com>
2019-05-03 18:41:39 +0430

    add GET route to auth route to authenticate users

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,20 @@
 const _ = require("lodash");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
+const Sequelize = require("sequelize");
+const Op = Sequelize.Op;
+const auth = require("../middleware/auth");
+const { Admin } = require("../startup/db");
 const express = require("express");
 router = express.Router();
 
-router.post("/", async (req, resp) => {});
+router.get("/", [auth], async (req, resp) => {
+  const userId = req.userId;
+
+  try {
+    const result = await Admin.findAll({ where: { id: { [Op.eq]: userId } } });
+    resp.send(result);
+  } catch (ex) {
+    throw ex.errors[0].message;
+  }
+});
 
 module.exports = router;
