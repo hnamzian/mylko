37874ef82bdf261b7b3d13861a46deb36f51b554
hnamzian <namazian87@gmail.com>
2019-05-03 17:59:22 +0430

    add employees router to express app

--- startup/routes.js
+++ startup/routes.js
@@ -3,6 +3,8 @@ require("express-async-errors");
 const deviceRouter = require("../routes/device");
 const milkingRouter = require("../routes/milking");
 const adminRouter = require("../routes/admin");
+const dairyRouter = require("../routes/dairy");
+const employeesRouter = require("../routes/employees");
 const authRouter = require("../routes/auth");
 const error = require("../middleware/error");
 
@@ -11,6 +13,8 @@ module.exports = function(app) {
   app.use("/device", deviceRouter);
   app.use("/milking", milkingRouter);
   app.use("/admin", adminRouter);
+  app.use("/dairy", dairyRouter);
+  app.use("/employees", employeesRouter);
   app.use("/auth", authRouter);
   app.use(error);
 };
