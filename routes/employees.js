b219621dea2963066e2245f3d8bd8d39dfbf803d
hnamzian <namazian87@gmail.com>
2019-05-03 17:55:08 +0430

    add sub-route /add to route employees to simply add new employee by user

--- routes/employees.js
+++ routes/employees.js
@@ -1,3 +1,21 @@
 const { Employees } = require("../startup/db");
+const config = require("config");
+const Joi = require("joi");
+const winston = require("winston");
+const _ = require("lodash");
 const express = require("express");
 router = express.Router();
+
+router.post("/add", async (req, resp) => {
+  const employee = _.pick(req.body, ["firstName", "lastName", "mobile", "email", "address", "position", "DairyId"]);
+
+  try {
+    const result = await Employees.create(employee);
+    resp.send(result);
+  } catch (ex) {
+    console.log(ex)
+    throw ex;
+  }
+});
+
+module.exports = router;
