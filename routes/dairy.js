a11d65bcb0a103e5b44557dd3bccef9bcbce7bb5
hnamzian <namazian87@gmail.com>
2019-05-03 17:46:54 +0430

    add sub-route /add to route dairy to simply add new dairy by user

--- routes/dairy.js
+++ routes/dairy.js
@@ -1,3 +1,19 @@
 const { Dairy } = require("../startup/db");
+const Joi = require("joi");
+const winston = require("winston");
+const _ = require("lodash");
 const express = require("express");
 router = express.Router();
+
+router.post("/add", async (req, resp) => {
+  const dairy = _.pick(req.body, ["name", "address", "AdminId"]);
+  
+  try {
+    const result = await Dairy.create(dairy);
+    resp.send(result);
+  } catch (ex) {
+    throw ex.errors[0].message;
+  }
+});
+
+module.exports = router;
