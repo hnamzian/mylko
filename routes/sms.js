92be73b5fc11cef10e362059609617207dcd6798
hnamzian <namazian87@gmail.com>
2019-05-05 14:42:33 +0430

    add _getSMSCode method for code generation + bug fixed at querying from sms table by mobile and expiredAt

--- routes/sms.js
+++ routes/sms.js
@@ -8,22 +8,8 @@ const router = express.Router();
 
 router.get("/", async (req, resp) => {
   const mobile = req.body.mobile;
-
-  let smsCode;
-  let expiredAt;
-
-  let lastToken = await SMSToken.findAll({
-    where: { expiredAt: { [Op.lte]: Date.now() } }
-  });
-
-  if (lastToken.length == 0) {
-    smsCode = getRandomInt(5);
-    expiredAt = Math.floor(Date.now() / 1000) + 10 * 60;
-    await SMSToken.create({ mobile, code: smsCode, expiredAt });
-  } else {
-    smsCode = lastToken[0].dataValues.code;
-    expiredAt = Date.now();
-  }
+  
+  const { smsCode, expiredAt } = await _getSMSCode(mobile)
 
   const smsToken = jwt.sign(
     {
@@ -39,6 +25,29 @@ router.get("/", async (req, resp) => {
     smsToken
   });
 });
+
+async function _getSMSCode(mobile) {
+  let smsCode;
+  let expiredAt;
+
+  let lastToken = await SMSToken.findAll({
+    where: {
+      mobile: { [Op.eq]: mobile },
+      expiredAt: { [Op.lte]: Date.now() }
+    }
+  });
+
+  if (lastToken.length == 0) {
+    smsCode = getRandomInt(5);
+    expiredAt = Math.floor(Date.now() / 1000) + 10 * 60;
+    await SMSToken.create({ mobile, code: smsCode, expiredAt });
+  } else {
+    smsCode = lastToken[0].dataValues.code;
+    expiredAt = Date.now();
+  }
+
+  return { smsCode, expiredAt }
+}
 
 function getRandomInt(length) {
   const min = 10 ** (length - 1);
