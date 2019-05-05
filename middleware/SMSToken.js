a998cb8fb3438e1bc54af2b604ce9bd1bbb662a3
hnamzian <namazian87@gmail.com>
2019-05-05 17:55:32 +0430

    add SMSToken middleware module to extract sms token data

---
+++ middleware/SMSToken.js
@@ -0,0 +1,24 @@
+const jwt = require("jsonwebtoken");
+const config = require("config");
+
+module.exports = function(req, res, next) {
+  const token = req.header("x-auth-token");
+  if (!token) return res.status(401).send("Access denied. token not provided");
+
+  try {
+    const decodedToken = jwt.verify(token, config.get("jwtPrivateKey"));
+    if (decodedToken.exp < Date.now() / 1000) {
+      return res.send({
+        success: false,
+        message: "Token is expired"
+      });
+    }
+
+    req.mobile = decodedToken.mobile;
+    req.expiredAt = decodedToken.exp;
+
+    next();
+  } catch (ex) {
+    res.status(400).send("invalid token");
+  }
+};
