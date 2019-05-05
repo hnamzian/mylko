461fd163f137d48e96b2d8b8b7eb9184fe2a0409
hnamzian <namazian87@gmail.com>
2019-05-05 11:45:45 +0430

    add getRandomInt method to generate fixed length random number as sms token

--- routes/sms.js
+++ routes/sms.js
@@ -2,4 +2,10 @@ const { SMSToken } = require("../startup/db");
 const express = require("express");
 const router = express.Router();
 
+function getRandomInt(length) {
+  const min = 10 ** (length-1);
+  const max = 10 ** length - 1;
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
 module.exports = router;
