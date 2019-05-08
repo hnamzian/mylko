4ce2767ac1fd6a53111898a235bec87804f49bdb
hnamzian <namazian87@gmail.com>
2019-05-08 14:17:01 +0430

    remove GET auth/ as auth by password wont be existed anymore

--- routes/auth.js
+++ routes/auth.js
@@ -6,16 +6,5 @@ const { Admin } = require("../startup/db");
 const express = require("express");
 router = express.Router();
 
-router.get("/", [auth], async (req, resp) => {
-  const userId = req.userId;
-
-  try {
-    const result = await Admin.findAll({ where: { id: { [Op.eq]: userId } } });
-    const user = _.omit(result[0].dataValues, ["password"]);
-    resp.send({ success: true, user });
-  } catch (ex) {
-    throw ex;
-  }
-});
 
 module.exports = router;
