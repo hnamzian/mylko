4597aaf5fd9dda927368d4b4a8967a661ddc6b79
hnamzian <namazian87@gmail.com>
2019-05-10 00:47:18 +0430

    remove routes implementation and add dairy/add using addDairy microservice

--- routes/dairy/dairy.js
+++ routes/dairy/dairy.js
@@ -1,34 +1,5 @@
-const { Dairy } = require("../../startup/db");
-const Joi = require("joi");
-const winston = require("winston");
-const _ = require("lodash");
+const addDairy = require("./addDairy");
 const express = require("express");
-router = express.Router();
+const router = express.Router();
 
-router.post("/add", async (req, resp) => {
-  const dairy = _.pick(req.body, ["name", "address", "AdminId"]);
-
-  const { error } = validate(dairy);
-  if (error) return resp.status(400).send("Joi: " + error.details[0].message);
-
-  try {
-    const result = await Dairy.create(dairy);
-    resp.send(result);
-  } catch (ex) {
-    throw ex.errors[0].message;
-  }
-});
-
-function validate(dairy) {
-  const schema = {
-    name: Joi.string()
-      .min(2)
-      .required(),
-    address: Joi.string().min(0),
-    AdminId: Joi.number()
-  };
-
-  return Joi.validate(dairy, schema);
-}
-
-module.exports = router;
+router.post("/add", addDairy);
