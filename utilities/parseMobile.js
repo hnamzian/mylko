d83780b4a363d2764f74835b215e818b49609c09
hnamzian <namazian87@gmail.com>
2019-05-09 17:06:56 +0430

    bug-fix add replaceAt method instead of undefined native method

--- utilities/parseMobile.js
+++ utilities/parseMobile.js
@@ -1,5 +1,9 @@
 module.exports = mobile => {
   if (!mobile.startsWith("0") && !mobile.startsWith("+98")) return false;
 
-  return mobile.startsWith("0") ? mobile.replaceAt(0, "+98") : mobile;
+  return mobile.startsWith("0") ? replaceAt(mobile, 0, "+98") : mobile;
 };
+
+function replaceAt(string, index, replace) {
+  return string.substring(0, index) + replace + string.substring(index + 1);
+}
