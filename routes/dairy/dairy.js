e8cbcbf26c5aa57cb457af0b2531393c8c82c30e
hnamzian <namazian87@gmail.com>
2019-05-10 01:51:59 +0430

    add DELETE /dairy/remove route to remove requested dairyId via removeDairy microservice

--- routes/dairy/dairy.js
+++ routes/dairy/dairy.js
@@ -2,6 +2,7 @@ const addDairy = require("./addDairy");
 const getDairy = require("./getDairy");
 const getDairies = require("./getDairies");
 const updateDairy = require("./updateDairy");
+const removeDairy = require("./removeDairy");
 const authTokenMW = require("../../middleware/auth");
 const express = require("express");
 const router = express.Router();
@@ -10,5 +11,6 @@ router.get("/", [authTokenMW], getDairy);
 router.get("/all", [authTokenMW], getDairies);
 router.post("/add", [authTokenMW], addDairy);
 router.put("/update", [authTokenMW], updateDairy);
+router.delete("/remove", [authTokenMW], removeDairy);
 
 module.exports = router;
