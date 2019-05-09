3027893669c5c8e657aed4710abc8567130cae3f
hnamzian <namazian87@gmail.com>
2019-05-10 01:15:19 +0430

    add GET /dairy route using getDairy microservice

--- routes/dairy/dairy.js
+++ routes/dairy/dairy.js
@@ -1,8 +1,10 @@
 const addDairy = require("./addDairy");
+const getDairy = require("./getDairy");
 const authTokenMW = require("../../middleware/auth");
 const express = require("express");
 const router = express.Router();
 
+router.get("/", [authTokenMW], getDairy);
 router.post("/add", [authTokenMW], addDairy);
 
 module.exports = router;
