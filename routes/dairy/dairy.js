fa9337964f958c97fca8b888e672af001629daee
hnamzian <namazian87@gmail.com>
2019-05-10 01:19:09 +0430

    add GET /dairy/all to return all dairies of userId using getAllDairies

--- routes/dairy/dairy.js
+++ routes/dairy/dairy.js
@@ -1,10 +1,12 @@
 const addDairy = require("./addDairy");
 const getDairy = require("./getDairy");
+const getDairies = require("./getDairies");
 const authTokenMW = require("../../middleware/auth");
 const express = require("express");
 const router = express.Router();
 
 router.get("/", [authTokenMW], getDairy);
+router.get("/all", [authTokenMW], getDairies);
 router.post("/add", [authTokenMW], addDairy);
 
 module.exports = router;
