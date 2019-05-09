26d8a198aacca2b3cc79ebcf5fc11c739ed61350
hnamzian <namazian87@gmail.com>
2019-05-10 01:40:52 +0430

    add PUT /dairy/update route to handle dairy udate via updateDairy microservice

--- routes/dairy/dairy.js
+++ routes/dairy/dairy.js
@@ -1,6 +1,7 @@
 const addDairy = require("./addDairy");
 const getDairy = require("./getDairy");
 const getDairies = require("./getDairies");
+const updateDairy = require("./updateDairy");
 const authTokenMW = require("../../middleware/auth");
 const express = require("express");
 const router = express.Router();
@@ -8,5 +9,6 @@ const router = express.Router();
 router.get("/", [authTokenMW], getDairy);
 router.get("/all", [authTokenMW], getDairies);
 router.post("/add", [authTokenMW], addDairy);
+router.put("/update", [authTokenMW], updateDairy);
 
 module.exports = router;
