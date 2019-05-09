d54e5d893bcf5158439e34210952375067f2ea1a
hnamzian <namazian87@gmail.com>
2019-05-10 00:49:54 +0430

    add authToken midleware to validate user and extract admin id

--- routes/dairy/dairy.js
+++ routes/dairy/dairy.js
@@ -1,5 +1,6 @@
 const addDairy = require("./addDairy");
+const authTokenMW = require("../../middleware/auth");
 const express = require("express");
 const router = express.Router();
 
-router.post("/add", addDairy);
+router.post("/add", [authTokenMW], addDairy);
