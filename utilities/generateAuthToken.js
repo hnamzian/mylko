45f051c643f0018abfd4ceaa841eb7d3576e9614
hnamzian <namazian87@gmail.com>
2019-05-09 15:32:17 +0430

    bug-fix import config and jwt module

--- utilities/generateAuthToken.js
+++ utilities/generateAuthToken.js
@@ -1,3 +1,6 @@
+const config = require("config");
+const jwt = require("jsonwebtoken");
+
 module.exports = adminId => {
   return jwt.sign({ id: adminId, isAdmin: this.isAdmin }, config.jwtPrivateKey);
 };
