07d727e244f197c599733409f0cb396f83f96e18
hnamzian <namazian87@gmail.com>
2019-05-10 18:37:31 +0430

    use updateDairy DAO module instead of raw query

--- routes/dairy/updateDairy.js
+++ routes/dairy/updateDairy.js
@@ -1,34 +1,22 @@
-const { Dairy } = require("../../startup/db");
+const updateDairyDAO = require("../../DAO/dairy/updateDairy");
 const validate = require("./_validate");
 const _ = require("lodash");
 
 module.exports = async (req, resp) => {
   const dairy = _.pick(req.body, ["id", "name", "address"]);
-  const adminId = req.userId;
+  dairy.AdminId = req.userId;
 
   const { error } = validate(dairy);
   if (error) return resp.status(400).send("Joi: " + error.details[0].message);
 
   try {
-    const [_, affectedRows] = await Dairy.update(dairy, {
-      where: { id: dairy.id, AdminId: adminId },
-      returning: true,
-      plain: true
-    });
+    let dairyData = await updateDairyDAO(dairy);
 
-    if (affectedRows) {
-      let dairyData = await Dairy.findOne({ where: { id: dairy.id, AdminId: adminId } });
-      return resp.send({
-        success: true,
-        message: "dairy updated",
-        dairy: dairyData
-      });
+    if (dairyData) {
+      return resp.send({ success: true, message: "dairy updated", dairy: dairyData });
     }
 
-    resp.send({
-      success: false,
-      message: "invalid dairy"
-    });
+    resp.send({ success: false, message: "invalid dairy" });
   } catch (ex) {
     throw ex.errors[0].message;
   }
