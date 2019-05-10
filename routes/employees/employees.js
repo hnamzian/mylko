211eddf13560daa3308a0380d716f0e7c71a6ead
hnamzian <namazian87@gmail.com>
2019-05-11 01:07:03 +0430

    add /employees/add route handled by addEmployee route handler

--- routes/employees/employees.js
+++ routes/employees/employees.js
@@ -1,4 +1,8 @@
+const AuthTokenMW = require("../../middleware/auth");
+const addEmployee = require("./addEmployee");
 const express = require("express");
 const router = express.Router();
+
+router.post("/add", [AuthTokenMW], addEmployee);
 
 module.exports = router;
