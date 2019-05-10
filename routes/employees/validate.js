4ad38ee035b0c6bd2180dc702b2cf78cc00a2d60
hnamzian <namazian87@gmail.com>
2019-05-11 01:16:05 +0430

    add routes/employees/validate as employee para validator

---
+++ routes/employees/validate.js
@@ -0,0 +1,27 @@
+const Joi = require("joi");
+
+module.exports = employee => {
+  const schema = {
+    firstName: Joi.string()
+      .min(2)
+      .required(),
+    lastName: Joi.string()
+      .min(2)
+      .required(),
+    mobile: Joi.string()
+      .min(11)
+      .max(13),
+    email: Joi.string()
+      .min(5)
+      .max(255)
+      .email(),
+    address: Joi.string().min(0),
+    position: Joi.string()
+      .min(5)
+      .max(255)
+      .required(),
+    DairyId: Joi.number().required()
+  };
+
+  return Joi.validate(employee, schema);
+};
