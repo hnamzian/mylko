36975db7c909cc89e945c138637bdd12fbb2dc52
hnamzian <namazian87@gmail.com>
2019-05-11 01:16:47 +0430

    validate received data on /employees/add request

--- routes/employees/addEmployee.js
+++ routes/employees/addEmployee.js
@@ -1,9 +1,13 @@
 const getDairyAdmin = require("../../DAO/dairy/getDairyAdmin");
 const addEmployeeDAO = require("../../DAO/employee/addEmployee");
+const validate = require("./validate");
 const _ = require("lodash");
 
 module.exports = async (req, resp) => {
   const employee = _.pick(req.body, ["firstName", "lastName", "mobile", "email", "address", "position", "DairyId"]);
+
+  const { error } = validate(employee);
+  if (error) return resp.status(400).send("Joi: " + error.details[0].message);
 
   const adminId = req.userId;
   const dairyAdmin = await getDairyAdmin(employee.DairyId);
