0ef274d043b02c47143b71fe88b1a81c13aeab65
hnamzian <namazian87@gmail.com>
2019-05-12 17:43:59 +0430

    add GET employees/ route to return employee profile

--- routes/employees/employees.js
+++ routes/employees/employees.js
@@ -1,8 +1,10 @@
 const AuthTokenMW = require("../../middleware/auth");
 const addEmployee = require("./addEmployee");
+const getEmplyee = require("./getEmployeeById");
 const express = require("express");
 const router = express.Router();
 
 router.post("/add", [AuthTokenMW], addEmployee);
+router.get("/", [AuthTokenMW], getEmplyee);
 
 module.exports = router;
