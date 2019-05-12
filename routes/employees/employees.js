fc434b1fb87c50c2b13508ad1f28c05f2929aff7
hnamzian <namazian87@gmail.com>
2019-05-12 18:25:23 +0430

    add GET employees/all to return employees of a dairy

--- routes/employees/employees.js
+++ routes/employees/employees.js
@@ -1,10 +1,12 @@
 const AuthTokenMW = require("../../middleware/auth");
 const addEmployee = require("./addEmployee");
 const getEmplyee = require("./getEmployeeById");
+const getEmployees = require("./getEmployees");
 const express = require("express");
 const router = express.Router();
 
 router.post("/add", [AuthTokenMW], addEmployee);
 router.get("/", [AuthTokenMW], getEmplyee);
+router.get("/all", [AuthTokenMW], getEmployees);
 
 module.exports = router;
