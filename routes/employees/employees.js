078f86dfe55c98102842bc8d7265fbd7aa12b288
hnamzian <namazian87@gmail.com>
2019-05-12 18:39:35 +0430

    add PUT employee/update to update an existing employee

--- routes/employees/employees.js
+++ routes/employees/employees.js
@@ -1,11 +1,13 @@
 const AuthTokenMW = require("../../middleware/auth");
 const addEmployee = require("./addEmployee");
+const updateEmployee = require("./updateEmployee");
 const getEmplyee = require("./getEmployeeById");
 const getEmployees = require("./getEmployees");
 const express = require("express");
 const router = express.Router();
 
 router.post("/add", [AuthTokenMW], addEmployee);
+router.put("/update", [AuthTokenMW], updateEmployee);
 router.get("/", [AuthTokenMW], getEmplyee);
 router.get("/all", [AuthTokenMW], getEmployees);
 
