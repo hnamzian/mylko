f5d982538c64a5af9085c76360d410625946cb36
hnamzian <namazian87@gmail.com>
2019-05-12 17:44:49 +0430

    bug-fix at adding adminId into employee object to update record + lint code

--- routes/employees/addEmployee.js
+++ routes/employees/addEmployee.js
@@ -4,7 +4,15 @@ const validate = require("./validate");
 const _ = require("lodash");
 
 module.exports = async (req, resp) => {
-  const employee = _.pick(req.body, ["firstName", "lastName", "mobile", "email", "address", "position", "DairyId"]);
+  const employee = _.pick(req.body, [
+    "firstName",
+    "lastName",
+    "mobile",
+    "email",
+    "address",
+    "position",
+    "DairyId"
+  ]);
 
   const { error } = validate(employee);
   if (error) return resp.status(400).send("Joi: " + error.details[0].message);
@@ -12,6 +20,7 @@ module.exports = async (req, resp) => {
   const adminId = req.userId;
   const dairyAdmin = await getDairyAdmin(employee.DairyId);
   if (dairyAdmin && dairyAdmin == adminId) {
+    employee.AdminId = adminId;
     const result = await addEmployeeDAO(employee);
     return resp.send({ success: true, message: "employee added", employee: result });
   }
