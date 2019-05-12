90d2d68a087214f6fb8cbe4a13c66697dea0760c
hnamzian <namazian87@gmail.com>
2019-05-12 18:45:35 +0430

    add DELETE route to remove an employee

--- routes/employees/employees.js
+++ routes/employees/employees.js
@@ -1,6 +1,7 @@
 const AuthTokenMW = require("../../middleware/auth");
 const addEmployee = require("./addEmployee");
 const updateEmployee = require("./updateEmployee");
+const removeEmployee = require("./removeEmployee");
 const getEmplyee = require("./getEmployeeById");
 const getEmployees = require("./getEmployees");
 const express = require("express");
@@ -8,6 +9,7 @@ const router = express.Router();
 
 router.post("/add", [AuthTokenMW], addEmployee);
 router.put("/update", [AuthTokenMW], updateEmployee);
+router.delete("/remove", [AuthTokenMW], removeEmployee);
 router.get("/", [AuthTokenMW], getEmplyee);
 router.get("/all", [AuthTokenMW], getEmployees);
 
