b758f7074ff7cba5e81214abbc5224291b6a3664
hnamzian <namazian87@gmail.com>
2019-05-28 01:56:35 +0430

    replace hard coded PUT /admin/update route handler by updateAdmin module

--- routes/admin/admin.js
+++ routes/admin/admin.js
@@ -6,6 +6,7 @@ const parseMobile = require("../../utilities/parseMobile");
 const Joi = require("joi");
 const _ = require("lodash");
 const getAdmin = require("./getAdmin");
+const updateAdmin = require("./updateAdmin");
 const express = require("express");
 router = express.Router();
 
@@ -36,46 +37,7 @@ router.post("/register", [SMSTokenMW], async (req, resp) => {
   }
 });
 
-router.put("/update", [AuthTokenMW], async (req, resp) => {
-  let admin = _.pick(req.body, ["firstName", "lastName", "mobile", "email", "address"]);
-
-  admin.mobile = parseMobile(admin.mobile);
-  if (!admin.mobile)
-    return resp.send({
-      success: false,
-      message: "invalid mobile number"
-    });
-
-  const { error } = validate(admin);
-  if (error) return resp.status(400).send("Joi: " + error.details[0].message);
-
-  try {
-    const [numberOfAffectedRows, affectedRows] = await Admin.update(admin, {
-      where: { id: req.userId, mobile: admin.mobile },
-      returning: true,
-      plain: true
-    });
-
-    if (affectedRows) {
-      let userData = await Admin.findOne({ where: { id: req.userId, mobile: admin.mobile } });
-      let user = userData.dataValues;
-      const token = generateAuthToken(user.id);
-      return resp.send({
-        success: true,
-        message: "user profile registered",
-        user,
-        token
-      });
-    }
-
-    resp.send({
-      success: false,
-      message: "invalid user"
-    });
-  } catch (ex) {
-    throw ex.errors[0].message;
-  }
-});
+router.put("/update", [AuthTokenMW], updateAdmin);
 
 router.get("/", [AuthTokenMW], getAdmin);
 
