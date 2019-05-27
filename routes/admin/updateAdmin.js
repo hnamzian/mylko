6a88be1ba6668f02dbd9fdb5ac5003b0ee6bd23a
hnamzian <namazian87@gmail.com>
2019-05-28 01:51:04 +0430

    add updateAdmin module as PUT admin/update route handler

---
+++ routes/admin/updateAdmin.js
@@ -0,0 +1,30 @@
+const validate = require("./_validate");
+const updateAdminDAO = require("../../DAO/admin/updateAdmin");
+const generateAuthToken = require("../../utilities/generateAuthToken");
+const parseMobile = require("../../utilities/parseMobile");
+const _ = require("lodash");
+
+module.exports = async (req, resp) => {
+  let admin = _.pick(req.body, ["firstName", "lastName", "mobile", "email", "address"]);
+
+  admin.mobile = parseMobile(admin.mobile);
+  if (!admin.mobile) return resp.send({ success: false, message: "invalid mobile number" });
+
+  const { error } = validate(admin);
+  if (error) return resp.status(400).send("Joi: " + error.details[0].message);
+
+  const adminProfile = await updateAdminDAO(admin);
+
+  if (adminProfile) {
+    const token = generateAuthToken(user.id);
+
+    return resp.send({
+      success: true,
+      message: "user profile registered",
+      admin: adminProfile,
+      token
+    });
+  }
+
+  resp.send({ success: false, message: "invalid user" });
+};
