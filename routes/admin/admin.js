da5b9852f7702e62e91d5af621316fb709a13a5f
hnamzian <namazian87@gmail.com>
2019-05-28 01:58:48 +0430

    remove validate method and not used module imports

--- routes/admin/admin.js
+++ routes/admin/admin.js
@@ -1,13 +1,9 @@
-const { Admin } = require("../../startup/db");
-const SMSTokenMW = require("../../middleware/SMSToken");
-const AuthTokenMW = require("../../middleware/auth");
-const generateAuthToken = require("../../utilities/generateAuthToken");
-const parseMobile = require("../../utilities/parseMobile");
-const Joi = require("joi");
-const _ = require("lodash");
 const getAdmin = require("./getAdmin");
 const updateAdmin = require("./updateAdmin");
 const registerAdmin = require("./registerAdmin");
+const SMSTokenMW = require("../../middleware/SMSToken");
+const AuthTokenMW = require("../../middleware/auth");
+const _ = require("lodash");
 const express = require("express");
 router = express.Router();
 
@@ -16,23 +12,5 @@ router.post("/register", [SMSTokenMW], registerAdmin);
 router.put("/update", [AuthTokenMW], updateAdmin);
 
 router.get("/", [AuthTokenMW], getAdmin);
-
-function validate(admin) {
-  const schema = {
-    firstName: Joi.string().min(2),
-    lastName: Joi.string().min(2),
-    mobile: Joi.string()
-      .min(11)
-      .max(13)
-      .required(),
-    email: Joi.string()
-      .min(5)
-      .max(255)
-      .email(),
-    address: Joi.string().min(0)
-  };
-
-  return Joi.validate(admin, schema);
-}
 
 module.exports = router;
