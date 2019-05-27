284ce6784b3af6c8bf2efe925ca3367e2b827a20
hnamzian <namazian87@gmail.com>
2019-05-28 01:57:43 +0430

    replace hard coded POST /admin/register route handler by registerAdmin module

--- routes/admin/admin.js
+++ routes/admin/admin.js
@@ -7,35 +7,11 @@ const Joi = require("joi");
 const _ = require("lodash");
 const getAdmin = require("./getAdmin");
 const updateAdmin = require("./updateAdmin");
+const registerAdmin = require("./registerAdmin");
 const express = require("express");
 router = express.Router();
 
-router.post("/register", [SMSTokenMW], async (req, resp) => {
-  let admin = _.pick(req.body, ["firstName", "lastName", "email", "address"]);
-
-  admin.mobile = parseMobile(req.mobile);
-  if (!admin.mobile)
-    return resp.send({
-      success: false,
-      message: "invalid mobile number"
-    });
-
-  const { error } = validate(admin);
-  if (error) return resp.status(400).send("Joi: " + error.details[0].message);
-
-  try {
-    const result = await Admin.create(admin);
-    const token = _generateAuthToken(result.dataValues.id);
-    return resp.send({
-      success: true,
-      message: "user profile registered",
-      user: result[0].dataValues,
-      token
-    });
-  } catch (ex) {
-    throw ex.errors[0].message;
-  }
-});
+router.post("/register", [SMSTokenMW], registerAdmin);
 
 router.put("/update", [AuthTokenMW], updateAdmin);
 
