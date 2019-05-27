8c16c66168a8b8cca597ff87850ec20fb27832b2
hnamzian <namazian87@gmail.com>
2019-05-27 13:02:58 +0430

    find admin profile from db and respond it on request

--- routes/auth/loginByToken.js
+++ routes/auth/loginByToken.js
@@ -1,3 +1,16 @@
+const findAdminByIdDAO = require("../../DAO/admin/findAdminById");
+const generateAuthToken = require("../../utilities/generateAuthToken");
+
 module.exports = async (req, resp) => {
-  
+  const userId = req.userId;
+  const admin = await findAdminByIdDAO(userId);
+  if (admin) {
+    const token = generateAuthToken(admin.id);
+    return resp.send({
+      success: true,
+      message: "Login successful",
+      user: admin,
+      token
+    });
+  }
 };
