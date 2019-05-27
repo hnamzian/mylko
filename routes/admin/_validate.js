de1eb298f5f98e4f4f2bf43aadd893fb2175f751
hnamzian <namazian87@gmail.com>
2019-05-28 01:35:31 +0430

    add _validate module to validate admin object

---
+++ routes/admin/_validate.js
@@ -0,0 +1,17 @@
+module.exports = function validate(admin) {
+  const schema = {
+    firstName: Joi.string().min(2),
+    lastName: Joi.string().min(2),
+    mobile: Joi.string()
+      .min(11)
+      .max(13)
+      .required(),
+    email: Joi.string()
+      .min(5)
+      .max(255)
+      .email(),
+    address: Joi.string().min(0)
+  };
+
+  return Joi.validate(admin, schema);
+};
