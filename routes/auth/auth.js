8444b332208bf1f45b62eefd33418385a81c4673
hnamzian <namazian87@gmail.com>
2019-05-27 13:01:50 +0430

    add GET auth/login route to authenticate admin user by token

--- routes/auth/auth.js
+++ routes/auth/auth.js
@@ -1,9 +1,12 @@
+const loginByToken = require("./loginByToken");
 const getSMSCode = require("./getSMSCode");
 const verifySMSCode = require("./verifySMSCode");
+const authTokenMW = require("../../middleware/auth");
 const SMSTokenMW = require("../../middleware/SMSToken");
 const express = require("express");
 const router = express.Router();
 
+router.get("/login", [authTokenMW], loginByToken);
 router.post("/get-sms-code", getSMSCode);
 router.post("/verify-sms-code", [SMSTokenMW], verifySMSCode);
 
