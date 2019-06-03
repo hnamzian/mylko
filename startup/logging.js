ba77092fa67fb9f18f2938ff712f531fbd93e2c5
hnamzian <namazian87@gmail.com>
2019-06-03 21:31:35 +0430

    lint logging module

--- startup/logging.js
+++ startup/logging.js
@@ -1,49 +1,52 @@
-const path = require('path')
-const winston = require('winston');
+const path = require("path");
+const winston = require("winston");
 const { timestamp, printf } = winston.format;
 
-
-module.exports = function () {
-
-    const errorFormatter = printf(error => {
-        const stack = ''
-        if (error.stack) {
-            error.stack.replace(/[\r\n]+/g, "")
-        }    
-        return `{"timestamp":"${error.timestamp}", "level":"${error.level}", "message":"${error.message}", "stack":"${stack}"}`;
-    });
-
-    const consoleFormatter = printf(error => {
-        return `${error.level}: ${error.message}`
-    })
-
-    winston.add(new winston.transports.File({ 
-        filename: path.join(__dirname, '../logs/exceptions.log'), 
-        format: winston.format.combine(
-            timestamp(),
-            errorFormatter),
-        level: 'error' 
-    }))
-    
-    winston.add(new winston.transports.File({ 
-        filename: path.join(__dirname, '../logs/info.log'), 
-        format: winston.format.combine(
-            timestamp(),
-            errorFormatter),
-        level: 'info' }))
-    
-    if (process.env.NODE_ENV !== 'production') {  
-        winston.add(new winston.transports.Console({
-            level: 'debug',
-            format: winston.format.combine( winston.format.colorize(), consoleFormatter)
-        }))
+module.exports = function() {
+  const errorFormatter = printf(error => {
+    const stack = "";
+    if (error.stack) {
+      error.stack.replace(/[\r\n]+/g, "");
     }
+    return `{"timestamp":"${error.timestamp}", "level":"${error.level}", "message":"${
+      error.message
+    }", "stack":"${stack}"}`;
+  });
 
-    process.on('uncaughtException', (ex) => {
-        winston.error({message: ex.message, stack: ex.stack})
+  const consoleFormatter = printf(error => {
+    return `${error.level}: ${error.message}`;
+  });
+
+  winston.add(
+    new winston.transports.File({
+      filename: path.join(__dirname, "../logs/exceptions.log"),
+      format: winston.format.combine(timestamp(), errorFormatter),
+      level: "error"
     })
-    
-    process.on('unhandledRejection', (ex) => {
-        throw new Error(ex)
+  );
+
+  winston.add(
+    new winston.transports.File({
+      filename: path.join(__dirname, "../logs/info.log"),
+      format: winston.format.combine(timestamp(), errorFormatter),
+      level: "info"
     })
-}
+  );
+
+  if (process.env.NODE_ENV !== "production") {
+    winston.add(
+      new winston.transports.Console({
+        level: "debug",
+        format: winston.format.combine(winston.format.colorize(), consoleFormatter)
+      })
+    );
+  }
+
+  process.on("uncaughtException", ex => {
+    winston.error({ message: ex.message, stack: ex.stack });
+  });
+
+  process.on("unhandledRejection", ex => {
+    throw new Error(ex);
+  });
+};
