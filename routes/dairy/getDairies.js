955e1543302e2a3a5d28a311398fc565ef687bad
hnamzian <namazian87@gmail.com>
2019-06-03 21:58:48 +0430

    throw Error on failure to be  handled at error middleware

--- routes/dairy/getDairies.js
+++ routes/dairy/getDairies.js
@@ -8,5 +8,5 @@ module.exports = async (req, resp) => {
   if (result && result.length)
     return resp.send({ success: true, message: "dairy found", dairies: result });
 
-  return resp.send({ success: false, message: "dairy not found" });
+  throw Error("dairy not found");
 };
